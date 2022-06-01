cf9d759c3f073200fd573f96cdf753e630841a78
Heneros <punk60826@gmail.com>
2022-06-01 03:21:32 +0300

    added fonts

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,21 +1,28 @@
 import Link from 'next/link';
 import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+font-weight: 700;
+font-size: 25px;
+line-height: 25px;
+`;
 
 export default function Header() {
     return (
         <header>
             <div class="first__line">
-                <Link href="/">Logo</Link>
+               <Logo><Link href="/">Logo</Link></Logo>
                 <div class="search__line">
                     <input type="search" placeholder="Enter value" />
                 </div>
                 <div class="phone__contact">
                     45-353459-3
-                </div> 
+                </div>
             </div>
             <div class="second__line">
-                <Nav />        
+                <Nav />
             </div>
-        </header>    
-    )    
+        </header>
+    )
 }
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+     @font-face{
+         font-family: 'gilroy';
+         src: url('Gilroy-Light.woff2') format('woff2');
+         font-weight: normal;
+         font-style: normal;
+     } 
+      body{
+           font-family: 'gilroy';
+   }
+`;
+
 export default function Page({ children }) {
     return (
-        <>
-        <Header/>
+        <div>
+            <GlobalStyles />
+            <Header />
             {children}
-        </>
+        </div>
     )
 }
 
