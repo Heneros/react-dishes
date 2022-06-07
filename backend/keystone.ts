1c14d4a03baab806dd6eafc2d666d9a9b1f9d4ba
Heneros <punk60826@gmail.com>
2022-06-08 02:20:53 +0300

    package.json updated with keystone.ts

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,7 +1,11 @@
-import 'dotenv/config';
+
+// import 'dotenv';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/react-dishes';
 
 const sessionConfig = {
     maxAge: 60 * 60 * 24 * 360,
+    // secret: process.env.COOKIE_SECRET
 }
