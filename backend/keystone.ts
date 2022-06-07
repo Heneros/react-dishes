52bbeb5df9003fd563d2272fd1954f0d85604c02
Heneros <punk60826@gmail.com>
2022-06-08 02:44:32 +0300

    keystone installed

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,11 +1,33 @@
+/*
+Welcome to Keystone! This file is what keystone uses to start the app.
 
-// import 'dotenv';
-import dotenv from 'dotenv'
-dotenv.config();
+It looks at the default export, and expects a Keystone config object.
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/react-dishes';
+You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
+*/
 
-const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 360,
-    // secret: process.env.COOKIE_SECRET
-}
+import { config } from '@keystone-6/core';
+
+// Look in the schema file for how we define our lists, and how users interact with them through graphql or the Admin UI
+import { lists } from './schema';
+
+// Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
+import { withAuth, session } from './auth';
+
+export default withAuth(
+  // Using the config function helps typescript guide you to the available options.
+  config({
+    // the db sets the database provider - we're using sqlite for the fastest startup experience
+    db: {
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
+    },
+    // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
+    ui: {
+      // For our starter, we check that someone has session data before letting them see the Admin UI.
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists,
+    session,
+  })
+);
