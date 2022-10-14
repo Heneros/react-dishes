bf9ada6c684c6489ed5775dc3a63d61dd5b6cac3
Heneros <punk60826@gmail.com>
2022-10-15 01:10:51 +0300

    updated

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,8 +1,22 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloServer } from "apollo-server";
 import mongoose from "mongoose";
 import { HASURA_KEY, HASURA_URL, MONGO_PASS } from "./keys";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
 const MONGODB = `mongodb+srv://admin:${MONGO_PASS}@reactdishescluster.0rln0.mongodb.net/?retryWrites=true&w=majority`;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// mongoose.connect(MONGODB, { useNewUrlParser: true })
+//     .then(() => {
+
+//     })
+
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
