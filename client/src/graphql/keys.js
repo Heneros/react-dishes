c76b87c9cbc71fbee44b6f00af43967eef1b51c7
Heneros <punk60826@gmail.com>
2022-10-21 01:34:46 +0300

    updated client folder

--- client/src/graphql/keys.js
+++
@@ -1,6 +0,0 @@
-export const HASURA_URL = 'https://valued-reptile-72.hasura.app/v1/graphql';
-export const HASURA_KEY = '5tC545xfBCszfUzMibwTdLnMSJ1mIYaCy4rSyfRyDxfEGgVKH0DsX1dmFCSVLd2M';
-export const MONGO_URI = 'mongodb+srv://admin:<password>@reactdishescluster.0rln0.mongodb.net/?retryWrites=true&w=majority'
-export const MONGO_PASS = 'xM88wQ7MBvBhsbfK';
-
-///valued-reptile
